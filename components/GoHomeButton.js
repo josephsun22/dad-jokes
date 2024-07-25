dad033082e6b18a232dd37012c4a411b71ca3457
Joseph Sun <155233774+josephsun22@users.noreply.github.com>
2024-07-25 22:09:36 +1000

    UX improvment on GoHome Button

--- components/GoHomeButton.js
+++ components/GoHomeButton.js
@@ -1,17 +1,17 @@
 import { useRouter } from "next/router";
-import { FaArrowLeft } from 'react-icons/fa';
+import { FaArrowLeft } from "react-icons/fa";
 
 const GoHomeButton = () => {
   const router = useRouter();
 
   const handleGoHome = () => {
-    router.back(); 
+    router.back();
   };
 
   return (
     <button
       onClick={handleGoHome}
-      className="fixed left-20 top-1/2 transform -translate-y-1/2 bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-full p-4 shadow-lg transition-transform duration-300 hover:translate-x-[-10px]"
+      className="fixed bottom-20 left-1/2 transform -translate-x-1/2 bg-blue-600 hover:bg-blue-700 text-white font-bold rounded-full p-4 shadow-lg transition-transform duration-300 hover:-translate-x-8"
     >
       <FaArrowLeft />
     </button>
